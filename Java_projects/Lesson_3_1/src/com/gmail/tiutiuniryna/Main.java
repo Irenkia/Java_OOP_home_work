a7760a401f1d1b5a4307f7238d96213c4e53b731
Iryna Tiutiun <tiutiuniryna@gmail.com>
2023-02-18 22:41:02 +0100

    Lesson_3_1_2 created a separate class that implements reading student characteristics with a keyboard

--- Java_projects/Lesson_3_1/src/com/gmail/tiutiuniryna/Main.java
+++ Java_projects/Lesson_3_1/src/com/gmail/tiutiuniryna/Main.java
@@ -1,37 +1,30 @@
 package com.gmail.tiutiuniryna;
 
-import java.util.Arrays;
-
 public class Main {
 
 	public static void main(String[] args) {
 		Gender male = Gender.MALE;
 		Gender female = Gender.FEMALE;
-		
-		Student student1 = new Student("Ivan","Ivanov",male,1,"group1");
-		Student student2 = new Student("Pete","Petrov",male,2,"group1");
-		Student student3 = new Student("Ola","Tkach",female,3,"group1");
-		Student student4 = new Student("Slava","Sidorov",male,4,"group1");
-		Student student5 = new Student("Sveta","Altuhova",female,5,"group1");
-//		System.out.println(student1);
-//		System.out.println(student2);
-//		System.out.println(student3);
-//		System.out.println(student4);
-//		System.out.println(student5);
-		
-		Group group1 =new Group();
+
+		Student student1 = new Student("Ivan", "Ivanov", male, 1, "group1");
+		Student student2 = new Student("Pete", "Petrov", male, 2, "group1");
+		Student student3 = new Student("Ola", "Tkach", female, 3, "group1");
+		Student student4 = new Student("Slava", "Sidorov", male, 4, "group1");
+		Student student5 = new Student("Sveta", "Altuhova", female, 5, "group1");
+
+		Group group1 = new Group();
 		try {
 			group1.addStudent(student1);
 			group1.addStudent(student2);
 			group1.addStudent(student3);
 			group1.addStudent(student4);
 			group1.addStudent(student5);
-		}catch(GroupOverflowException e) {
+		} catch (GroupOverflowException e) {
 			e.printStackTrace();
 		}
 		System.out.println(group1);
 		System.out.println();
-		
+
 		System.out.println(group1.removeStudentByID(4));
 		System.out.println(group1);
 		System.out.println();
@@ -39,16 +32,19 @@ public class Main {
 		try {
 			group1.searchStudentByLastName("Tkach");
 			System.out.println(student3);
-			Student student6 = new Student("Lena","Polakova",female,6,"group1");
+			Student student6 = new Student("Lena", "Polakova", female, 6, "group1");
 			group1.searchStudentByLastName(student6.getLastName());
 			System.out.println(student6);
-		}catch(StudentNotFoundException e) {
+		} catch (StudentNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 		System.out.println(group1.sortingStudentsByName());
-		
+
 		System.out.println(group1.sortStudentsByLastName());
+
+		Student student7 = ReadingCharacteristics.createStudent();
+		System.out.println(student7);
 
 	}
 
---
+++ Java_projects/Lesson_3_1/src/com/gmail/tiutiuniryna/ReadingCharacteristics.java
@@ -0,0 +1,74 @@
+package com.gmail.tiutiuniryna;
+
+import java.util.Scanner;
+
+public class ReadingCharacteristics {
+	public static Student createStudent() {
+		Scanner sc = new Scanner(System.in);
+
+		String name = nameScValidation(sc);
+		String lastName = lastNameScValidation(sc);
+		Gender gender = genderScValidation(sc);
+		int id = idScValidation(sc);
+		System.out.println();
+		String groupName = groupNameScValidation(sc);
+		sc.close();
+
+		Student student = new Student(name, lastName, gender, id, groupName);
+		return student;
+	}
+
+	private static String nameScValidation(Scanner sc) {
+		String nameValidate;
+		System.out.println("Enter name student");
+		while (!sc.hasNextLine()) {
+			sc.nextLine();
+			System.out.println("Invalid data entered, please try again");
+		}
+		nameValidate = sc.nextLine();
+		return nameValidate;
+	}
+
+	private static String lastNameScValidation(Scanner sc) {
+		String lastNameValidate;
+		System.out.println("Enter last name student");
+		while (!sc.hasNextLine()) {
+			sc.nextLine();
+			System.out.println("Invalid data entered, please try again");
+		}
+		lastNameValidate = sc.nextLine();
+		return lastNameValidate;
+	}
+
+	private static Gender genderScValidation(Scanner sc) {
+		String gender;
+		System.out.println("Enter the gender of the student");
+		System.out.println("enter only \"male\" or \"female\"");
+		gender = sc.nextLine().toUpperCase();
+		Gender genderValidate = Gender.valueOf(gender);
+		return genderValidate;
+	}
+
+	private static int idScValidation(Scanner sc) {
+		int idValidate;
+		System.out.println("Enter id student (integer from 1 to 100)");
+		while (!sc.hasNextInt() || (idValidate = sc.nextInt()) > 100 || idValidate < 1) {
+			sc.nextLine();
+			System.out.println("Invalid data entered, please try again");
+		}
+		System.out.println();
+		return idValidate;
+	}
+
+	private static String groupNameScValidation(Scanner sc) {
+		String groupNameValidate;
+		System.out.println("Enter group name student");
+		while (!sc.hasNextLine()) {
+			sc.nextLine();
+			System.out.println("Invalid data entered, please try again");
+		}
+		groupNameValidate = sc.nextLine();
+		return groupNameValidate;
+	}
+
+}
